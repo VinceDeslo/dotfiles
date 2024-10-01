f320403f6c2ad2e2d5172ada523bc1b097ebcffa
Vincent Desloover <vincent.desloover.dev@gmail.com>
2024-09-30 20:40:27 -0400

    run home manager and add starship as a first test

--- home.nix
+++ home.nix
@@ -1,8 +1,10 @@
 {pkgs, ...}: {
 	home.username = "vince";
-	home.homeDirectory = "/home/vince";
+	home.homeDirectory = "/Users/vince";
 	home.stateVersion = "24.05";
 	programs.home-manager.enable = true;
 	
-	home.packages = [];
+	home.packages = [
+		pkgs.starship
+	];
 }
